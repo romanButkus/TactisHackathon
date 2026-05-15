c67554e7588106a03a4ae09a2a34061193d82150
p1xlized <apaduret@pixlized.net>
2026-05-16 00:11:53 +0300

    ADD - OpenCV

--- devenv.nix
+++ devenv.nix
@@ -3,4 +3,22 @@
 {
   languages.python.enable = true;
   languages.python.venv.enable = true;
+
+  packages = [
+    pkgs.zlib
+    pkgs.libGL
+    pkgs.glib
+    pkgs.xorg.libxcb      # Adds libxcb.so.1
+    pkgs.xorg.libX11      # Common follow-up requirement
+    pkgs.stdenv.cc.cc.lib
+  ];
+
+  env.LD_LIBRARY_PATH = "${lib.makeLibraryPath (with pkgs; [
+    stdenv.cc.cc.lib
+    zlib
+    libGL
+    glib
+    xorg.libxcb
+    xorg.libX11
+  ])}";
 }
